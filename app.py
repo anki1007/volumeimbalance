84f9b49814a33856108290cd7956a9220745b66a
anki1007 <anki.agarwal23@gmail.com>
2026-01-06 00:32:06 +0530

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# app.py
+
 from flask import Flask, render_template
 from scanner import scan
 
@@ -7,3 +9,6 @@ app = Flask(__name__)
 def index():
     data = scan()
     return render_template("index.html", data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
